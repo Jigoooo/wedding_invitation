45c9f4c7fab9a97c2a142910a5bf247556794f29
kimjigoo <jiwook98@naver.com>
2023-12-18 21:52:28 +0900

    first push

--- node_back/index.js
+++ node_back/index.js
@@ -1,2 +1,13 @@
-const options = '';
-console.log(options);
+import express from 'express';
+
+const app = express();
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
