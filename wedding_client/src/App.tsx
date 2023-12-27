48f73a3df17fe1cb508d1ba06c2df11978d1e746
kimjigoo <jiwook98@naver.com>
2023-12-28 00:06:27 +0900

    first push

--- wedding_client/src/App.tsx
+++ wedding_client/src/App.tsx
@@ -4,8 +4,12 @@ import './App.css';
 import WeddingMainPage from './pages/main/WeddingMainPage';
 
 function App() {
+  const handleContextMenu: React.MouseEventHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className={'main-container'}>
+    <div className={'main-container'} onContextMenu={handleContextMenu}>
       <WeddingMainPage />
     </div>
   );
